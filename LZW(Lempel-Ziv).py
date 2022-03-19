834d4025bf07525acbf2170d543c4a893cbc8776
Arman Hossain <64675035+Arman170616@users.noreply.github.com>
2022-03-19 21:55:19 +0600

    Create LZW(Lempel-Ziv).py

---
+++ LZW(Lempel-Ziv).py
@@ -0,0 +1,20 @@
+input_str = 'AABABBBABAABABBBABBABB' 
+
+keys_dict = {}
+
+ind = 0
+inc = 1
+while True:
+    if not (len(input_str) >= ind+inc):
+        break
+    sub_str = input_str[ind:ind + inc]
+    #print(sub_str,ind,inc)
+    if sub_str in keys_dict:
+        inc += 1
+    else:
+        keys_dict[sub_str] = 0
+        ind += inc
+        inc = 1
+        
+a= list(keys_dict)
+print(a)
